84d5362b796e32c547f8886b383795f58d8b481b
Aman0050 <amannaeem78654@gmail.com>
2026-04-15 15:03:10 +0530

    Delete server directory

--- server/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUsers, getAnalytics } = require('../controllers/userController');
-const { protect, authorize } = require('../middleware/authMiddleware');
-
-router.get('/', protect, authorize('admin'), getUsers);
-router.get('/analytics', protect, authorize('admin'), getAnalytics);
-
-module.exports = router;
