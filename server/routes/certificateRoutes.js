4220d12fba620bd589de613f39e1291f3e362d31
Aman0050 <amannaeem78654@gmail.com>
2026-04-26 20:42:06 +0530

    Delete server directory

--- server/routes/certificateRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateCertificate } = require('../controllers/certificateController');
-const { protect, authorize } = require('../middleware/authMiddleware');
-
-router.get('/:eventId/download', protect, authorize('student'), generateCertificate);
-
-module.exports = router;
